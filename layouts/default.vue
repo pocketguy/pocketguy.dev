96cd5d7f2477731dbba7f040f0bbc4274bcb9835
pocketguy <pocketguy@protonmail.com>
2019-11-07 23:32:58 +0300

    fix main page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,82 @@
 <template>
   <div class="main">
-    <nav>
-      <p class="brand h1">pocketguy.dev</p>
+    <nav class="navbar">
+      <p class="brand">
+        <nuxt-link :to="{ name: 'index' }">
+          <img class="logo" src="@/assets/logo.svg" alt="pocketguy.dev" />
+        </nuxt-link>
+      </p>
       <ul class="nav">
         <li class="nav-item"></li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Проекты</a>
+          <a class="nav-link" href="#">проекты</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Блог</a>
+          <a class="nav-link" href="#">блог</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">GitHub</a>
+          <a class="nav-link" href="#">github</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Контакты</a>
+          <a class="nav-link" href="#">контакты</a>
         </li>
         <li class="nav-item"></li>
       </ul>
     </nav>
     <nuxt />
+    <footer class="footer">
+      <ul class="nav">
+        <li class="nav-item"></li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">проекты</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">блог</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">github</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">контакты</a>
+        </li>
+        <li class="nav-item"></li>
+      </ul>
+      <p class="brand">
+        <nuxt-link :to="{ name: 'index' }">
+          <img class="logo" src="@/assets/logo.svg" alt="pocketguy.dev" />
+        </nuxt-link>
+      </p>
+      <p class="text-center">
+        copyright © {{ new Date().getFullYear() }} pocketguy@protonmail.com
+      </p>
+    </footer>
   </div>
 </template>
 
 <style lang="stylus" scoped>
 .main {
-  padding-top: rythm(4);
+  padding: rythm(4);
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.footer {
+  margin-top: auto;
 }
 
 .brand {
-  text-align: center;
   padding: rythm(4);
+
+  & + p {
+    margin-top: 0;
+  }
+
+  .logo {
+    display: block;
+    margin: auto;
+    height: rythm(8);
+  }
 }
 
 .nav {
@@ -49,6 +95,7 @@
     }
 
     .nav-link {
+      letter-spacing: 1.5px;
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,37 @@
 <template>
   <div class="container-fluid-screen">
     <h1>pocketguy.dev</h1>
-    <p>
-      <img src="/icon.png" alt="pocketguy.dev" />
-    </p>
-    <p>
-      <nuxt-link :to="{ name: 'typo' }">typography</nuxt-link>
-    </p>
+    <p>сайт веб-разработчика</p>
+    <div class="row">
+      <div class="col-1">
+        <ul class="list-style-none">
+          <li>
+            <h2><a href="#">проекты</a></h2>
+          </li>
+          <li>
+            <h2><a href="#">блог</a></h2>
+          </li>
+        </ul>
+      </div>
+      <div class="col-2">
+        <p>
+          <img src="/icon.png" alt="pocketguy.dev icon" />
+        </p>
+      </div>
+      <div class="col-1 text-right">
+        <ul class="list-style-none">
+          <li>
+            <h2><a href="#">github</a></h2>
+          </li>
+          <li>
+            <h2><a href="#">контакты</a></h2>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <h2>
+      <nuxt-link :to="{ name: 'typo' }">типографика</nuxt-link>
+    </h2>
   </div>
 </template>
 
@@ -29,13 +54,33 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.list-style-none {
+  li:before {
+    display: none;
+  }
+}
+
 .container-fluid-screen {
-  width: 100%;
-  min-height: 100vh;
+  margin: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  .row {
+    display: flex;
+    flex-direction: row;
+    margin-top: rythm(8);
+
+    for fl in (1 .. 2) {
+      .col-{fl} {
+        flex: fl;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+      }
+    }
+  }
 
   img {
     max-width: 100%;
