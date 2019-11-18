6e2a59559d8ab92dd42a54d158d4bf693327788c
pocketguy <pocketguy@protonmail.com>
2019-11-18 23:31:42 +0300

    added basic about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <p><nuxt-link :to="{ name: 'index' }">главная</nuxt-link> - обо мне</p>
-    <h1>Обо мне</h1>
+    <!-- eslint-disable-next-line -->
+    <div class="xs-mt-8" v-html="mdDocument.html" />
   </div>
 </template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      mdDocument: require('@/data/about.md').default
+    }
+  }
+}
+</script>
